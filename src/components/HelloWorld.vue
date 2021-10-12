caa9ccdef4bc2a3951d55df8daef488595038405
Ahmad Irfan <wajibdiisi@gmail.com>
2021-10-12 12:03:18 +0700

    init push

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,59 @@
+<template>
+
+  <MDBContainer>
+
+    <div
+      class="d-flex justify-content-center align-items-center"
+      style="height: 100vh"
+    >
+    <MDBPopconfirm>
+    Default
+  </MDBPopconfirm>
+      <div class="text-center">
+        <img
+          class="mb-4"
+          src="https://mdbootstrap.com/img/logo/mdb-earlyaccess.png"
+          style="width: 400px; height: 190px"
+        />
+        <h5 class="mb-3">{{ msg }}</h5>
+        <p>
+          This Early Access grants you access to a UI Kit that is still under
+          development.
+        </p>
+        <p>
+          The schedule for all upcoming features can be found in
+          <a href="https://mdbootstrap.com/docs/b5/roadmap/">release roadmap</a
+          >.
+        </p>
+        <p class="mb-3">MDB Team</p>
+        <a
+          class="btn btn-primary btn-lg"
+          href="https://mdbootstrap.com/docs/b5/vue/pro/installation/"
+          target="_blank"
+          role="button"
+          >Start using MDB</a
+        >
+      </div>
+    </div>
+  </MDBContainer>
+</template>
+
+<script>
+import { MDBContainer } from "mdb-vue-ui-kit";
+import { MDBPopconfirm } from "mdb-vue-ui-kit";
+export default {
+  name: "HelloWorld",
+  components: {
+    MDBContainer,
+    MDBPopconfirm
+  },
+  props: {
+    msg: String
+  }
+};
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <HelloWorld
+    msg="Thank you for using our product. We're glad you're with us."
+  />
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld
+  }
+};
+</script>

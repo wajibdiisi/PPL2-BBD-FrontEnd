2fc6da7916759152925d6626578098e61aa9cbf7
Ahmad Irfan <wajibdiisi@gmail.com>
2021-10-13 14:48:45 +0700

    update

---
+++ src/components/Wisatadetails.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <MDBContainer>
+    <MDBRow>
+      <MDBCol>One of three columns</MDBCol>
+      <MDBCol>One of three columns</MDBCol>
+      <MDBCol>One of three columns</MDBCol>
+    </MDBRow>
+  </MDBContainer>
+</template>
+<script>
+  import { MDBCol, MDBRow, MDBContainer} from 'mdb-vue-ui-kit';
+
+
+  export default {
+    components: {
+      MDBCol,
+      MDBRow,
+      MDBContainer
+    },
+  };
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
-  <navbar />
+  <Navbar />
+  <Wisatadetails />
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from "@/components/HelloWorld.vue";
 import Navbar from "../components/Navbarcopy.vue";
+import Wisatadetails from "../components/Wisatadetails.vue";
 
 export default {
   name: "Home",
   components: {
     Navbar,
+    Wisatadetails
   },
 };
 </script>

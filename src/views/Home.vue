ee02dd532d0d476617b6fb2ad71b17bf0ec73a79
GedeBagus <Bdarmagita@gmail.com>
2021-10-14 01:53:23 +0700

    Homepage
    Hampir kelar, tinggal bagusin searchbox kalo bisa.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,371 @@
 <template>
   <Navbar />
+  <MDBContainer> </MDBContainer>
+  <div class="habib">
+    <img
+      src="https://www.akseleran.co.id/blog/wp-content/uploads/2020/08/Ilustrasi-Wisata-Bali-Sumber-The-Jakarta-Post.png"
+      class="search-bg"
+      alt=""
+    />
+    <div class="search-box">
+      <p class="search-text">Cari Wisata</p>
+      <input class="form-control" type="text" placeholder="Cari Wisata" />
+    </div>
+  </div>
+  <div class="container" style="margin-bottom: 10vh">
+    <div class="row">
+      <h5>Tempat yang sering di kunjungi</h5>
+      <p>Jelajahi daftar pilihan tempat populer di Indonesia</p>
+      <div class="col-md-3">
+        <div class="card bagud">
+          <img
+            class="card-img-top"
+            src="https://www.akseleran.co.id/blog/wp-content/uploads/2020/08/Ilustrasi-Wisata-Bali-Sumber-The-Jakarta-Post.png"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <a href="#" class="btn btn-primary">See Details</a>
+          </div>
+          <div class="card-isi">
+            <p>Candi Borobudur</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Accusantium corporis aut aliquid quasi a voluptatibus nihil iusto
+              quo similique. Ad.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <MDBCard>
+          <MDBCardImg
+            src="https://www.akseleran.co.id/blog/wp-content/uploads/2020/08/Ilustrasi-Wisata-Bali-Sumber-The-Jakarta-Post.png"
+            top
+            alt="..."
+          />
+          <MDBCardBody>
+            <MDBCardTitle>Candi Borobudur</MDBCardTitle>
+            <MDBCardText>
+              Candi Borobudur adalah candi Buddha terbesar di dunia. 
+            </MDBCardText>
+            <MDBBtn tag="a" href="#!" color="primary">See Details</MDBBtn>
+          </MDBCardBody>
+        </MDBCard>
+      </div>
+      <div class="col-md-3">
+          <MDBCard>
+          <MDBCardImg
+            src="https://www.akseleran.co.id/blog/wp-content/uploads/2020/08/Ilustrasi-Wisata-Bali-Sumber-The-Jakarta-Post.png"
+            top
+            alt="..."
+          />
+          <MDBCardBody>
+            <MDBCardTitle>Candi Borobudur</MDBCardTitle>
+            <MDBCardText>
+              Candi Borobudur adalah candi Buddha terbesar di dunia. 
+            </MDBCardText>
+            <MDBBtn tag="a" href="#!" color="primary">See Details</MDBBtn>
+          </MDBCardBody>
+        </MDBCard>
+      </div>
+      <div class="col-md-3">
+          <MDBCard>
+          <MDBCardImg
+            src="https://www.akseleran.co.id/blog/wp-content/uploads/2020/08/Ilustrasi-Wisata-Bali-Sumber-The-Jakarta-Post.png"
+            top
+            alt="..."
+          />
+          <MDBCardBody>
+            <MDBCardTitle>Candi Borobudur</MDBCardTitle>
+            <MDBCardText>
+              Candi Borobudur adalah candi Buddha terbesar di dunia. 
+            </MDBCardText>
+            <MDBBtn tag="a" href="#!" color="primary">See Details</MDBBtn>
+          </MDBCardBody>
+        </MDBCard>
+      </div>
+    </div>
+  </div>
+  <div class="container" style="margin-bottom: 10vh">
+    <div class="row">
+      <h5>Lihat Diskusi Mengenai Tempat Wisata</h5>
+      <p>Temukan pendapat orang yang pernah ke destinasi wisata yang akan anda kunjungi</p>
+      <div class="col-md-3">
+        <div class="card bagud">
+          <img
+            class="card-img-top"
+            src="https://www.akseleran.co.id/blog/wp-content/uploads/2020/08/Ilustrasi-Wisata-Bali-Sumber-The-Jakarta-Post.png"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <a href="#" class="btn btn-primary">See Details</a>
+          </div>
+          <div class="card-isi">
+            <p>Candi Borobudur</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Accusantium corporis aut aliquid quasi a voluptatibus nihil iusto
+              quo similique. Ad.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <MDBCard>
+          <MDBCardImg
+            src="https://www.akseleran.co.id/blog/wp-content/uploads/2020/08/Ilustrasi-Wisata-Bali-Sumber-The-Jakarta-Post.png"
+            top
+            alt="..."
+          />
+          <MDBCardBody>
+            <MDBCardTitle>Candi Borobudur</MDBCardTitle>
+            <MDBCardText>
+              Candi Borobudur adalah candi Buddha terbesar di dunia. 
+            </MDBCardText>
+            <MDBBtn tag="a" href="#!" color="primary">See Details</MDBBtn>
+          </MDBCardBody>
+        </MDBCard>
+      </div>
+      <div class="col-md-3">
+          <MDBCard>
+          <MDBCardImg
+            src="https://www.akseleran.co.id/blog/wp-content/uploads/2020/08/Ilustrasi-Wisata-Bali-Sumber-The-Jakarta-Post.png"
+            top
+            alt="..."
+          />
+          <MDBCardBody>
+            <MDBCardTitle>Candi Borobudur</MDBCardTitle>
+            <MDBCardText>
+              Candi Borobudur adalah candi Buddha terbesar di dunia. 
+            </MDBCardText>
+            <MDBBtn tag="a" href="#!" color="primary">See Details</MDBBtn>
+          </MDBCardBody>
+        </MDBCard>
+      </div>
+      <div class="col-md-3">
+          <MDBCard>
+          <MDBCardImg
+            src="https://www.akseleran.co.id/blog/wp-content/uploads/2020/08/Ilustrasi-Wisata-Bali-Sumber-The-Jakarta-Post.png"
+            top
+            alt="..."
+          />
+          <MDBCardBody>
+            <MDBCardTitle>Candi Borobudur</MDBCardTitle>
+            <MDBCardText>
+              Candi Borobudur adalah candi Buddha terbesar di dunia. 
+            </MDBCardText>
+            <MDBBtn tag="a" href="#!" color="primary">See Details</MDBBtn>
+          </MDBCardBody>
+        </MDBCard>
+      </div>
+    </div>
+  </div>
+  <div class="container" style="margin-bottom: 10vh">
+    <div class="row">
+      <h5>Lihat Moment Mengenai Tempat Wisata</h5>
+      <p>Temukan moment terbaik dari tempat wisata yang akan anda kunjungi</p>
+      <div class="col-md-3">
+        <div class="card bagud">
+          <img
+            class="card-img-top"
+            src="https://www.akseleran.co.id/blog/wp-content/uploads/2020/08/Ilustrasi-Wisata-Bali-Sumber-The-Jakarta-Post.png"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <a href="#" class="btn btn-primary">See Details</a>
+          </div>
+          <div class="card-isi">
+            <p>Candi Borobudur</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Accusantium corporis aut aliquid quasi a voluptatibus nihil iusto
+              quo similique. Ad.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <MDBCard>
+          <MDBCardImg
+            src="https://www.akseleran.co.id/blog/wp-content/uploads/2020/08/Ilustrasi-Wisata-Bali-Sumber-The-Jakarta-Post.png"
+            top
+            alt="..."
+          />
+          <MDBCardBody>
+            <MDBCardTitle>Candi Borobudur</MDBCardTitle>
+            <MDBCardText>
+              Candi Borobudur adalah candi Buddha terbesar di dunia. 
+            </MDBCardText>
+            <MDBBtn tag="a" href="#!" color="primary">See Details</MDBBtn>
+          </MDBCardBody>
+        </MDBCard>
+      </div>
+      <div class="col-md-3">
+          <MDBCard>
+          <MDBCardImg
+            src="https://www.akseleran.co.id/blog/wp-content/uploads/2020/08/Ilustrasi-Wisata-Bali-Sumber-The-Jakarta-Post.png"
+            top
+            alt="..."
+          />
+          <MDBCardBody>
+            <MDBCardTitle>Candi Borobudur</MDBCardTitle>
+            <MDBCardText>
+              Candi Borobudur adalah candi Buddha terbesar di dunia. 
+            </MDBCardText>
+            <MDBBtn tag="a" href="#!" color="primary">See Details</MDBBtn>
+          </MDBCardBody>
+        </MDBCard>
+      </div>
+      <div class="col-md-3">
+          <MDBCard>
+          <MDBCardImg
+            src="https://www.akseleran.co.id/blog/wp-content/uploads/2020/08/Ilustrasi-Wisata-Bali-Sumber-The-Jakarta-Post.png"
+            top
+            alt="..."
+          />
+          <MDBCardBody>
+            <MDBCardTitle>Candi Borobudur</MDBCardTitle>
+            <MDBCardText>
+              Candi Borobudur adalah candi Buddha terbesar di dunia. 
+            </MDBCardText>
+            <MDBBtn tag="a" href="#!" color="primary">See Details</MDBBtn>
+          </MDBCardBody>
+        </MDBCard>
+      </div>
+    </div>
+  </div>
+  <Footer />
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from "@/components/HelloWorld.vue";
 import Navbar from "../components/Navbarcopy.vue";
+import Footer from "../components/Footer.vue";
 //import Wisatadetails from "../components/Wisatadetails.vue";
+// import { ref } from "vue";
+import {
+  // MDBCarousel,
+  MDBContainer,
+  // MDBInput,
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardImg,
+  MDBBtn,
+} from "mdb-vue-ui-kit";
 
 export default {
   name: "Home",
   components: {
     Navbar,
+    Footer,
     //Wisatadetails
+    // MDBCarousel,
+    MDBContainer,
+    MDBCard,
+    MDBCardBody,
+    MDBCardTitle,
+    MDBCardText,
+    MDBCardImg,
+    MDBBtn,
+    // MDBInput,
   },
+  // setup() {
+  //   const items1 = [
+  //     {
+  //       src: "https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg",
+  //       alt: "...",
+  //       label: "First slide label",
+  //       caption: "Liburan lebih nikmat dengan MyTour!",
+  //     },
+  //     {
+  //       src: "https://mdbootstrap.com/img/Photos/Slides/img%20(22).jpg",
+  //       alt: "...",
+  //       label: "Second slide label",
+  //       caption: "Liburan lebih nikmat dengan MyTour!",
+  //     },
+  //     {
+  //       src: "https://mdbootstrap.com/img/Photos/Slides/img%20(23).jpg",
+  //       alt: "...",
+  //       label: "Third slide label",
+  //       caption: "Liburan lebih nikmat dengan MyTour!",
+  //     },
+  //   ];
+  //   const carousel1 = ref(0);
+
+  //   return {
+  //     items1,
+  //     // carousel1,
+  //   };
+  // },
 };
 </script>
+<style>
+.navbar {
+  background-color: rgba(255, 255, 255, 0.561);
+}
+.nav-link {
+  color: black !important;
+}
+.habib {
+  width: 70vw;
+  margin: 0 auto;
+  background: black;
+  overflow: hidden;
+  border-radius: 10px;
+  position: relative;
+  margin-bottom: 10vh;
+}
+.habib .search-bg {
+  object-fit: cover;
+  opacity: 0.6;
+}
+.search-bg {
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
+.search-box {
+  position: absolute;
+  top: 35vh;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 2;
+  width: 50%;
+}
+.form-control {
+  height: 60px;
+}
+.search-text {
+  font-size: 5rem;
+  color: white;
+  font-weight: 700;
+  text-align: center;
+}
+.bagud {
+  position: relative;
+}
+.card-isi {
+  position: absolute;
+  background-color: white;
+  top: 150px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 15vw;
+}
+.card-body {
+  background-color: white;
+  height: 250px;
+}
+.btn {
+  position: absolute;
+  top: 330px;
+  left: 50%;
+  transform: translateX(-50%);
+}
+* {
+  font-family: "Montserrat", sans-serif;
+}
+</style>
